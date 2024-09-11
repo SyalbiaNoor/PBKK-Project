999546b3918762e904326c2e29048f35e074c721
Syalbia Noor <syalbiaishere@gmail.com>
2024-09-12 04:15:44 +0700

    Initial commit

---
+++ coba1/resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title> {{ $title }} </x-slot:title>
+    <h3> Contact page testing </h3>
+  </x-layout>
---
+++ coba1/resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+  <x-slot:title> {{ $title }} </x-slot:title>
+  <h3> Home page testing </h3>
+</x-layout>
